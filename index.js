74e32fd9a5ceafd85225cab85334f9e8e3374cad
shahzad akram <alyakram786@gmail.com>
2024-01-12 17:28:07 +0500

    making divs

---
+++ index.js
@@ -0,0 +1,96 @@
+// table of  19
+let multi15 = document.getElementById("multi_15");
+
+for(let i = 1 ; i <= 20 ; i++){
+
+    console.log('15 *', i ,'=', i*15);
+    multi15.innerHTML += `15 * ${i} = ${i*15} <br>`;
+    multi15.style.backgroundColor = "orange";
+}
+
+
+
+
+
+// table of  16
+let multi16 = document.getElementById("multi_16");
+
+for(let i = 1 ; i <= 20 ; i++){
+
+    console.log('16 *', i ,'=', i*16);
+    multi16.innerHTML += `16 * ${i} = ${i*16} <br>`;
+    multi16.style.backgroundColor = "grey";
+}
+
+
+
+
+
+
+
+// table of  17
+let multi17 = document.getElementById("multi_17");
+
+for(let i = 1 ; i <= 20 ; i++){
+
+    console.log('17 *', i ,'=', i*17);
+    multi17.innerHTML += `17 * ${i} = ${i*17} <br>`;
+    multi17.style.backgroundColor = "blue";
+}
+
+
+
+
+
+
+
+
+// table of  19
+let multi18 = document.getElementById("multi_18");
+
+for(let i = 1 ; i <= 20 ; i++){
+
+    console.log('18 *', i ,'=', i*18);
+    multi18.innerHTML += `18 * ${i} = ${i*18} <br>`;
+    multi18.style.backgroundColor = "green";
+}
+
+
+
+
+
+
+
+// table of  19
+let multi19 = document.getElementById("multi_19");
+
+for(let i = 1 ; i <= 20 ; i++){
+
+    console.log('19 *', i ,'=', i*19);
+    multi19.innerHTML += `19 * ${i} = ${i*19} <br>`;
+    multi19.style.backgroundColor = "yellow";
+}
+
+
+
+   //table of 20 
+   let multi20 = document.getElementById("multi_20");
+
+   for(let i = 1 ; i <= 20 ; i++){
+
+    console.log('20 *', i,'=', i*20)
+    multi20.innerHTML += `20 * ${i} = ${i*20}<br>`
+    multi20.style.backgroundColor = "pink"
+   
+}
+
+
+    //table of 21 
+let multi21 = document.getElementById("multi_21");
+
+   for(let i = 1 ; i <= 20 ; i++){
+
+    console.log('21 *', i,'=', i*21)
+    multi21.innerHTML += `21 * ${i} = ${i*21}<br>`
+   
+}
